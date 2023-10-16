309e6a1afcf09024e1a347a2ba68cdee6bbc1b66
kimeuni <axdc1234@naver.com>
2023-10-16 17:53:17 +0900

    20231016 1753 자바스크립트 연습..

--- javascript/1013/test.js
+++ javascript/1013/test.js
@@ -1,6 +1,7 @@
 /* test.js */
 
 // 작업의 계속 유무 체크
+// 메세지창을 통해서 진행여부 결정하기(confirm())
 function ynCheck() {
     yn = confirm("작업을 계속 하시겠습니까?");  // true와 false로 구분한다.. (확인(true)/취소(false))
     if(yn) {
